e14b63ccf8bf6880c50a05e74b6d1356a7079232
Mary Giles Beeson <marybeeson23@gmail.com>
2022-06-13 13:26:00 -0400

    added setup for boss and android and items

--- Game_Repository/Androids_Bosses.cs
+++ Game_Repository/Androids_Bosses.cs
@@ -2,6 +2,33 @@ namespace Game_Repository;
 
     public class Androids : LivingCreature
     {
-        public string Name 
+        public Androids()
+        {
+
+        }
+        public Androids(string AndroidName, int AndroidAtkDMG)
+        {
+            this.AndroidName = AndroidName;
+            this.AndroidAtkDMG = AndroidAtkDMG;
+        }
+        public string AndroidName { get; set; }
+        public int AndroidAtkDMG { get; set; }
         
+            
+        
+    }
+
+    public class Bosses : LivingCreature 
+    {
+        public Bosses()
+        {
+
+        }
+        public Bosses(string BossName, int BossAtkDMG)
+        {
+            this.BossName = BossName;
+            this.BossAtkDMG = BossAtkDMG;
+        }
+        public string BossName { get; set; }
+        public int BossAtkDMG { get; set; }
     }
---
+++ Game_Repository/Items.cs
@@ -0,0 +1,15 @@
+namespace Game_Repository
+{
+    public class Items
+    {
+        public Items()
+        {
+
+        }
+        public Items(string Dragonball)
+        {
+            this.Dragonball = Dragonball;
+        }
+        public string Dragonball { get; set; }
+    }
+}
