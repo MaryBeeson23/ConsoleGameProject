869eabea7c88be6c7a180e88b681df18dd9f762e
Mary Giles Beeson <marybeeson23@gmail.com>
2022-06-13 10:59:02 -0400

    setting up the hero

--- Game_Repository/Class1.cs
+++ Game_Repository/Class1.cs
@@ -1,5 +1,36 @@
 ﻿namespace Game_Repository;
-public class Class1
-{
 
+
+
+public class TJ : Saiyen
+{
+    public TJ()
+    {
+
+    }
+    public TJ()
+    {
+        this.Name = Name;
+        this.Level = Level;
+        this.HP = HP;
+        this.Weapon = Weapon;
+
+
+    }
+    public string Name (get; set; )
+    
+    public int Level (get; set; )
+
+    public int HP (get; set; )
+
+    public enum Weapon (get; set; )
+
+
+}
+
+public enum Weapon {
+    PowerPole,
+    BraveSword,
+    SickleOfSorrow,
+    KameHameHa
 }
