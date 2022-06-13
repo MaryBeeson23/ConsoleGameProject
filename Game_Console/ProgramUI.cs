fbd3c77e389e0696477d79cbc7bf136eb8966432
Jalen Waggoner <jalenwaggoner@gmail.com>
2022-06-13 19:48:00 -0400

    Added EnemyDamage Method. Removed Bosses and HealingChamber Classes.

--- Game_Console/ProgramUI.cs
+++ Game_Console/ProgramUI.cs
@@ -22,9 +22,9 @@ namespace Game_Console
                 Androids androids0 = new Androids("Android 13", 5);
                 Androids androids1 = new Androids("Android 15", 5);
                 Androids androids2 = new Androids("Android 17", 5);
-                Bosses bosses0 = new Bosses("Cell", 1000);
-                Bosses bosses1 = new Bosses("Frieza", 2000);
-                Bosses bosses2 = new Bosses("Vegeta", 3000);
+                Androids bosses0 = new Androids("Cell", 1000);
+                Androids bosses1 = new Androids("Frieza", 2000);
+                Androids bosses2 = new Androids("Vegeta", 3000);
             }
 
             private void PlayerHeal(TJ _tJ){
@@ -35,15 +35,27 @@ namespace Game_Console
                 }
             }
 
-            private void TJDamage(TJ tj, Androids androids){
-                int damageToAndroid = tj.WeaponDMG;
+            private void TJDamage(TJ tJ, Androids androids){
+                int weaponDMG = tJ.WeaponDMG;
                 int androidCurrentHP = androids.CurrentHP;
                 
-                if (androidCurrentHP <= damageToAndroid){
+                if (androidCurrentHP <= weaponDMG){
                 androidCurrentHP = 0;
                 } 
                 else {
-                    androidCurrentHP = androidCurrentHP - damageToAndroid;
+                    androidCurrentHP = androidCurrentHP - weaponDMG;
+                }
+            }
+
+            private void EnemyDamage(TJ tJ, Androids androids){
+                int androidAttackDMG = androids.AndroidAtkDMG;
+                int tJCurrentHP = tJ.CurrentHP;
+                
+                if (tJCurrentHP <= androidAttackDMG){
+                tJCurrentHP = 0;
+                } 
+                else {
+                    tJCurrentHP = tJCurrentHP - androidAttackDMG;
                 }
             }
     }
--- Game_Repository/Androids_Bosses.cs
+++ Game_Repository/Androids_Bosses.cs
@@ -14,18 +14,3 @@ namespace Game_Repository;
         public string AndroidName { get; set; }
         public int AndroidAtkDMG { get; set; }
     }
-
-    public class Bosses : LivingCreature 
-    {
-        public Bosses() : base()
-        {
-
-        }
-        public Bosses(string BossName, int BossAtkDMG) : base (MaxHP, CurrentHP)
-        {
-            this.BossName = BossName;
-            this.BossAtkDMG = BossAtkDMG;
-        }
-        public string BossName { get; set; }
-        public int BossAtkDMG { get; set; }
-    }
