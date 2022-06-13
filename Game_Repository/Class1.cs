9e8c99d79687eab0aee14b6705facbe91c066a03
Mary Giles Beeson <marybeeson23@gmail.com>
2022-06-13 11:28:40 -0400

    switch case for wepons

--- Game_Repository/Class1.cs
+++ Game_Repository/Class1.cs
@@ -2,13 +2,13 @@
 
 
 
-public class TJ : Saiyen
+public class TJ : LivingCreature
 {
     public TJ()
     {
 
     }
-    public TJ()
+    public TJ(string Name, int Level, int HP)
     {
         this.Name = Name;
         this.Level = Level;
@@ -16,15 +16,35 @@ public class TJ : Saiyen
         this.Weapon = Weapon;
 
 
+
     }
-    public string Name (get; set; )
+    public string Name {get; set; }
     
-    public int Level (get; set; )
+    public int Level {get; set; }
 
-    public int HP (get; set; )
+    public int HP {get; set; }
 
-    public enum Weapon (get; set; )
-
+    public Weapon Weapon {get; set; }
+    public int WeaponDMG {get {
+        switch (Weapon)
+        {
+            case Weapon.PowerPole:
+                return 50;
+                break;
+            case Weapon.BraveSword:
+                return 75;
+                break;
+            case Weapon.SickleOfSorrow:
+                return 100;
+                break;
+            case Weapon.KameHameHa:
+                return 1000;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    } }
 
 }
 
